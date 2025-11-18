6eff46b1ce1d331430947e9dd890006800a0808d
Trystan Henriques <trystanhenriquesgit@gmail.com>
2025-11-18 14:44:47 -0500

    Add PersonalInfo inner class to UserData
    Introduces the PersonalInfo inner class within UserData, encapsulating user attributes such as age, weight, height, activity level, gender, and date of entry. Includes constructors, getters, setters, and a helper method to parse date strings into Calendar objects. Not including the setter and getter for the dateOfEntry.

---
+++ UserData.java
@@ -0,0 +1,61 @@
+import java.util.Calendar;
+
+
+public class UserData {
+
+    private class PersonalInfo {
+        int age;
+        int weight_lbs;
+        Calendar dateOfEntry;
+        int height_cm;
+        int activityLevel;
+        char gender; // Male (m) or Female (f)
+        
+        // Constructor
+        public PersonalInfo(int age, int weight, int height, int activityLevel, char gender, String dateOfEntryString)
+        {
+            this.age = age;
+            this.weight_lbs = weight;
+            this.height_cm = height;
+            this.activityLevel = activityLevel;
+            this.gender = gender;
+
+            // Turn the date of entry string into a valid date
+            constructDateOfEntry(dateOfEntryString);
+
+        }
+
+        // small helper mthod to convert a dateOfEnteryString to a valid dateOfEntry calendar object
+        private void constructDateOfEntry(String dateOfEnteryString) {
+            dateOfEntry = Calendar.getInstance();
+            dateOfEntry.clear();                                         // Epoch
+
+            String[] dateOfEntryTokens = dateOfEnteryString.split("/");    // Turn the String into tokens
+
+            // Parse each part into integers
+            int month = Integer.parseInt(dateOfEntryTokens[0]) - 1;        // Months Starts with 0
+            int day = Integer.parseInt(dateOfEntryTokens[1]);
+            int year = Integer.parseInt(dateOfEntryTokens[2]);
+
+            // Set the date
+            dateOfEntry.set(year, month, day);                           // Set the Calendar object based on the fields
+        }
+
+        // getters
+        public int getAge() {return age;}
+        public int getWeight_lbs() {return weight_lbs;}
+        public int getHeight_cm() {return height_cm;}
+        public int getActivityLevel() {return activityLevel;}
+        public char getGender() {return gender;}
+
+        // setters
+        public void setAge(int age) {this.age = age;}
+        public void setWeight_lbs(int weight) {this.weight_lbs = weight;}
+        public void setHeight_cm(int height) {this.height_cm = height;}
+        public void setActivityLevel(int activityLevel) {this.activityLevel = activityLevel;}
+        public void setGender(char gender) {this.gender = gender;}
+
+        
+
+    }
+}
